7b4214405bafba4079b185d8dc7cbc2502b90256
CrhistianBadillo <100823791+CrhistianBadillo@users.noreply.github.com>
2022-06-05 16:55:22 -0500

    calense perros
    a ver que pex

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '@/views/login'
+Vue.use(Router)
+
+export default new Router({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/',
+      redirect: '/dashboard',
+      component: () => import('@/layouts/Layout'),
+      children: [
+        {
+          name: 'Alerts',
+          path: 'pages/alerts',
+          component: () => import('@/views/pages/Profile')
+        },
+        {
+          name: 'Profile',
+          path: 'pages/profile',
+          component: () => import('@/views/pages/Profile')
+        },
+        {
+          name: 'Icons',
+          path: 'pages/icons'
+        },
+        {
+          name: 'TableSimple',
+          path: 'pages/tables-simple'
+        },
+         {
+          name: 'Corte',
+          path: 'pages/Corte'
+        },
+        {
+          name: 'Dashboard',
+          path: '/dashboard',
+          component: () => import('@/views/Dashboard')
+        }
+      ]
+    }
+  ]
+})
